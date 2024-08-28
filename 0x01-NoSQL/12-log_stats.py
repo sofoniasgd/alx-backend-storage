639b154a963005a771e1aa263c082992e759ba81
sofoniasgd <zephaniahsofa@gmail.com>
2024-08-28 16:52:06 +0300

    updated task 12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -6,16 +6,15 @@ from pymongo import MongoClient
 
 
 if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    coll = client.logs.nginx
+    coll = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
     # coll will be the collection
-    print(coll.count(), "logs")
+    print(coll.counti_documents({}), "logs")
     print("Methods:")
-    print("\tmethod GET:", coll.count({"method": "GET"}))
-    print("\tmethod POST:", coll.count({"method": "POST"}))
-    print("\tmethod PUT:", coll.count({"method": "PUT"}))
-    print("\tmethod PATCH:", coll.count({"method": "PATCH"}))
-    print("\tmethod DELETE:", coll.count({"method": "DELETE"}))
+    print("\tmethod GET:", coll.count_documents({"method": "GET"}))
+    print("\tmethod POST:", coll.count_documents({"method": "POST"}))
+    print("\tmethod PUT:", coll.count_documents({"method": "PUT"}))
+    print("\tmethod PATCH:", coll.count_documents({"method": "PATCH"}))
+    print("\tmethod DELETE:", coll.count_documents({"method": "DELETE"}))
     count = coll.aggregate([
       {$match: {"method": "GET", "path": "/status"}},
       {$count: "count"}
